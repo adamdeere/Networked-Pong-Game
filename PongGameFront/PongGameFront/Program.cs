046da731c5574fea9e4bb6554d63347c38f5eea4
Adam <adam.deere@hotmail.com>
2022-07-06 23:34:52 +0100

    Added new open tk project
     to bring it up to date with modern open TK

---
+++ PongGameFront/PongGameFront/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using PongGameFront;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello there!");
+
+var nativeWindowSettings = new NativeWindowSettings()
+{
+    Size = new Vector2i(800, 600),
+    Title = "LearnOpenTK - Creating a Window",
+    // This is needed to run on macos
+    Flags = ContextFlags.ForwardCompatible,
+};
+
+// To create a new window, create a class that extends GameWindow, then call Run() on it.
+using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+{
+    window.Run();
+}
