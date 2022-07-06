dfdb35cf13aeed06c706ea2c4515a1893744ddd9
Adam <adam.deere@hotmail.com>
2022-07-06 19:57:23 +0100

    Replaced text render code for text render class
    As I said... second year code...

--- 08241_ACW2/New_Lab1/Scenes/MainMenuScene.cs
+++ 08241_ACW2/New_Lab1/Scenes/MainMenuScene.cs
@@ -1,23 +1,20 @@
 ﻿using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
-using System;
-using System.Drawing;
-using System.Drawing.Imaging;
+using PongGame.Utils;
 
 namespace PongGame
 {
     internal class MainMenuScene : Scene, IScene
     {
-        private Bitmap textBMP;
-        private int textTexture;
-        private Graphics textGFX;
         public static string serverIp;
         public static string masterIp;
         public static int portNo;
 
         private Client client; //= new Client(serverIp, portNo, masterIp);
         private MasterServer master;
+
+        private RenderText mTextRender;
 
         public MainMenuScene(SceneManager sceneManager, Client inClient, MasterServer inMaster)
             : base(sceneManager)
@@ -32,19 +29,7 @@ namespace PongGame
             // Set the Render and Update delegates to the Update and Render methods of this class
             sceneManager.renderer = Render;
             sceneManager.updater = Update;
-
-            // Create Bitmap and OpenGL texture for rendering text
-            textBMP = new Bitmap(sceneManager.Width, sceneManager.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb); // match window size
-            textGFX = Graphics.FromImage(textBMP);
-            textGFX.Clear(Color.CornflowerBlue);
-            textTexture = GL.GenTexture();
-            GL.Enable(EnableCap.Texture2D);
-            GL.BindTexture(TextureTarget.Texture2D, textTexture);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Linear);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Linear);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, textBMP.Width, textBMP.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, IntPtr.Zero);
-            GL.BindTexture(TextureTarget.Texture2D, 0);
-            GL.Disable(EnableCap.Texture2D);
+            mTextRender = new RenderText(sceneManager.Width, sceneManager.Height);
         }
 
         public void Update(FrameEventArgs e)
@@ -61,40 +46,14 @@ namespace PongGame
             GL.LoadIdentity();
             GL.Ortho(0, sceneManager.Width, 0, sceneManager.Height, -1, 1);
 
-            if (textBMP != null)
-            {
-                RenderText("welcome to pong", 0f, 0f);
-                RenderText("1. single player game", 0f, 40f);
-                RenderText("2. multyplayer game", 0f, 80f);
-                RenderText("3. host networked game", 0f, 140f);
-                RenderText("4 connect to networked game", 0f, 180f);
-                RenderText("5. display high scores", 0, 220f);
-            }
+            mTextRender.renderTextOnScreen("welcome to pong", 0f, 0f);
+            mTextRender.renderTextOnScreen("1. single player game", 0f, 40f);
+            mTextRender.renderTextOnScreen("2. multyplayer game", 0f, 80f);
+            mTextRender.renderTextOnScreen("3. host networked game", 0f, 140f);
+            mTextRender.renderTextOnScreen("4 connect to networked game", 0f, 180f);
+            mTextRender.renderTextOnScreen("5. display high scores", 0, 220f);
+           
         }
-
-        private void RenderText(string text, float x, float y)
-        {
-            textGFX.DrawString(text, new Font("Arial", 20), Brushes.White, x, y);
-
-            // Enable the texture
-            GL.Enable(EnableCap.Texture2D);
-            GL.BindTexture(TextureTarget.Texture2D, textTexture);
-
-            BitmapData data = textBMP.LockBits(new Rectangle(0, 0, textBMP.Width, textBMP.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, (int)textBMP.Width, (int)textBMP.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-            textBMP.UnlockBits(data);
-
-            GL.Begin(PrimitiveType.Quads);
-            GL.TexCoord2(0f, 1f); GL.Vertex2(0f, 0f);
-            GL.TexCoord2(1f, 1f); GL.Vertex2(sceneManager.Width, 0f);
-            GL.TexCoord2(1f, 0f); GL.Vertex2(sceneManager.Width, sceneManager.Height);
-            GL.TexCoord2(0f, 0f); GL.Vertex2(0f, sceneManager.Height);
-            GL.End();
-
-            GL.BindTexture(TextureTarget.Texture2D, 0);
-            GL.Disable(EnableCap.Texture2D);
-        }
-
         public void SelectMenu(KeyboardKeyEventArgs e)
         {
             KeyboardState keyState = Keyboard.GetState();
@@ -108,7 +67,7 @@ namespace PongGame
             }
             if (keyState.IsKeyDown(Key.Number3))// host networked game
             {
-                RenderText("waiting for a connection...", 0, 260f);
+                mTextRender.renderTextOnScreen("waiting for a connection...", 0, 260f);
                 master.confirmConnection();
                 if (client.lineFromSlave.Contains("confrim"))
                 {
